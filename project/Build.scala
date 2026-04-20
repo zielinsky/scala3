a632faf63abf5ef443d7a0f4c122346a97813905
Tomasz Godzik <tgodzik@users.noreply.github.com>
2026-04-20 11:27:37 +0200

    feature: Add logical package parsing for sourcepath (#25446)
    The idea is that not all files follow the directory structure, which
    would make the current directory parsing (probably) inefficient. By
    creating the full package structure we also make sure that only relevant
    files are always loaded and no recalculation is needed.
    
    I might be totally wrong here, so would love some feedback if it's even
    necessary. I've put it behind a flag just in case.
    
    <!-- if the PR is still a WIP, create it as a draft PR (or convert it
    into one) -->
     
    ## How much have your relied on LLM-based tools in this contribution?
    
    Moderately -> LLM helped rewrite the code from the Scala 2
    implementation.
    
    ## How was the solution tested?
    
    Automatic test added to the presentation compiler tests
    
    ## Additional notes
    
    I started porting changes that Julian Dragos did in Metals v2 and I
    realized that Scala 3 compiler is already quite efficient with loading
    symbols from source path, so I ported the other change used for
    determining package structure to use it efficiently in loading correct
    files from the source path
    
    
    <!-- Placeholder for any extra context regarding this contribution. -->
    
    <!--
    When in doubt, and for support regarding contributions to a particular
    component of the compiler,
    refer to [our contribution
    guide](https://github.com/scala/scala3/blob/main/CONTRIBUTING.md),
    and feel free to tag the maintainers listed there for the area(s) you
    are modifying.
    -->
    
    
    **EDIT** I also added the actual handling of sourcepath from Metals and
    run benchmarks on it.

--- project/Build.scala
+++ project/Build.scala
@@ -1292,7 +1292,6 @@ object Build {
       Compile / resourceGenerators += generateLibraryProperties.taskValue,
       bspEnabled := enableBspAllProjects,
       Compile / mainClass := None,
-
       Test / unmanagedSourceDirectories   := Seq(baseDirectory.value / "test"),
       Test / unmanagedResourceDirectories := Seq(baseDirectory.value / "test-resources"),
       libraryDependencies ++= Seq(
@@ -2200,7 +2199,7 @@ object Build {
       BuildInfoPlugin.buildInfoDefaultSettings
 
   lazy val presentationCompilerSettings = {
-    val mtagsVersion = "1.6.3"
+    val mtagsVersion = "1.6.7"
     Seq(
       libraryDependencies ++= Seq(
         "org.lz4" % "lz4-java" % "1.8.0",
